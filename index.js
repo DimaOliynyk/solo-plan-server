6b1447a45410e92d73fe3e5aaadf531a8a8333ea
Dmytro Oliinyk <d.oliynyk@outlook.com>
2025-10-27 13:52:20 +0100

    fix

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ mongoose
 app.use(express.json());
 app.use(volleyball);
 app.use(helmet());
-app.use(
-  cors({
-    origin: '*', // Replace '*' with your frontend URL in production
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  })
-);
+// CORS configuration
+const corsOptions = {
+  origin: "*", // Replace with your frontend URL in production
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Pre-flight request handling\=
 
 // Passport JWT & Google OAuth
 passport.use(
